2abafaf4cba791bb1c7c750316af29822047b315
devleandrodias <leandrodbdias@gmail.com>
2021-12-21 11:09:31 -0300

    read and write files using node fs

--- 06-node-express-api/files/file-upload.js
+++ 06-node-express-api/files/file-upload.js
@@ -3,12 +3,21 @@ const path = require("path");
 
 const getPathFile = (file) => path.resolve(__dirname, "..", "assets", file);
 
-function writeFileNewDog() {}
-
-fs.readFile(getPathFile("dog.jpg"), (err, data) => {
-  if (err) console.error(err);
-
-  fs.writeFile(getPathFile("new-dog.jpg"), data, (err) => {
+function writeFileNewDogUsingBuffers() {
+  fs.readFile(getPathFile("dog.jpg"), (err, data) => {
     if (err) console.error(err);
+
+    fs.writeFile(getPathFile("buffer-dog.jpg"), data, (err) => {
+      if (err) console.error(err);
+    });
   });
-});
+}
+
+function writeFileNewDogUsingStreams() {
+  fs.createReadStream(getPathFile("dog.jpg"))
+    .pipe(fs.createWriteStream(getPathFile("stream-dog.jpg")))
+    .on("finish", () => console.log("Dog Stream Finish..."));
+}
+
+writeFileNewDogUsingStreams(); // Async
+writeFileNewDogUsingBuffers(); // Sync
